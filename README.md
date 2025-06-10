f745a719f8f8239927777b5f0902695c062c199f
snowsan0113 <egawayuuki0113@gmail.com>
2025-06-10 20:29:50 +0900

    【追加】プラグインを追加
    【追加】プラグインを追加
    
    【追加】プラグインを追加

---
+++ README.md
@@ -0,0 +1,5 @@
+# Discord_Connect
+* このプラグインはDiscordとMinecraftを連携することができるプラグインです。
+
+## 使用方法
+* DiscordでBotを取得する。# Discord_Connect
---
+++ src/main/java/snowsan0113/discord_connect/Main.java
@@ -0,0 +1,16 @@
+package snowsan0113.discord_connect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("プラグインが有効になりました。");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
